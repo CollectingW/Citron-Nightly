2c277475370628e90c2de0f8eb5502de5e998573
CollectingW <ward9128@gmail.com>
2025-09-27 16:27:17 -0400

    fix: get-dependencies.sh
    Change due to source code

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -2,8 +2,8 @@
 
 set -ex
 ARCH="$(uname -m)"
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
+# This command installs all the full, official dependencies needed to build AND package Citron.
 pacman -Syu --noconfirm \
 	base-devel          \
 	boost               \
@@ -11,25 +11,35 @@ pacman -Syu --noconfirm \
 	catch2              \
 	cmake               \
 	curl                \
+	enet                \
 	fmt                 \
+	ffmpeg              \
 	gamemode            \
 	gcc                 \
 	git                 \
+	glslang             \
+	glu                 \
+	hidapi              \
+	libdecor            \
+	libvpx              \
 	libxi               \
 	libxkbcommon-x11    \
 	libxss              \
 	mbedtls2            \
 	mesa                \
+	nasm                \
 	ninja               \
+	nlohmann-json       \
+	numactl             \
 	openal              \
-	pipewire-audio      \
 	pulseaudio          \
 	pulseaudio-alsa     \
 	qt6-base            \
-	qt6ct               \
+	qt6-networkauth     \
 	qt6-multimedia      \
 	qt6-tools           \
 	qt6-wayland         \
+	qt6-translations    \
 	sdl2                \
 	unzip               \
 	vulkan-headers      \
@@ -42,52 +52,3 @@ pacman -Syu --noconfirm \
 	xorg-server-xvfb    \
 	zip                 \
 	zsync
-
-echo "Installing debloated packages..."
-echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-mesa qt6-base-mini llvm-libs-nano opus-nano
-
-echo "Building citron..."
-echo "---------------------------------------------------------------"
-sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
-sed -i 's|-O2|-O3|; s|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|; s|#MAKEFLAGS|MAKEFLAGS|' /etc/makepkg.conf
-cat /etc/makepkg.conf
-
-if [ "$1" = 'v3' ] && [ "$ARCH" = 'x86_64' ]; then
-	echo "Making x86-64-v3 optimized build of citron..."
-	ARCH_FLAGS="-march=x86-64-v3 -O3"
-elif [ "$ARCH" = 'x86_64' ]; then
-	echo "Making x86-64 generic build of citron..."
-	ARCH_FLAGS="-march=x86-64 -mtune=generic -O3"
-else
-	echo "Making aarch64 build of citron..."
-	ARCH_FLAGS="-march=armv8-a -mtune=generic -O3"
-fi
-
-if [ "$DEVEL" = 'true' ]; then
-	citronpkg=citron-git
-	echo "Making nightly build..."
-else
-	citronpkg=citron
-	echo "Making stable build..."
-fi
-
-git clone https://aur.archlinux.org/"$citronpkg".git ./citron
-cd ./citron
-
-sed -i \
-	-e "s|x86_64|$ARCH|g"                              \
-	-e 's|DISCORD_PRESENCE=ON|DISCORD_PRESENCE=OFF|'   \
-	-e 's|USE_QT_MULTIMEDIA=ON|USE_QT_MULTIMEDIA=OFF|' \
-	-e 's|BUILD_TYPE=None|BUILD_TYPE=Release|'         \
-	-e "s|\$CXXFLAGS|$ARCH_FLAGS|g"                    \
-	-e "s|\$CFLAGS|$ARCH_FLAGS|g"                      \
-	./PKGBUILD
-cat ./PKGBUILD
-
-makepkg -fs --noconfirm --skippgpcheck
-ls -la .
-pacman --noconfirm -U ./*.pkg.tar.*
-pacman -Q "$citronpkg" | awk '{print $2; exit}' > ~/version
